e69d8fa4e42f038f92fad6eaf3ffc24e20bdf071
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-05-01 15:56:25 -0700

    Adds placeholder Header component and spacing

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+function Header(){
+    return (
+        <header className="header">
+            <div className="header__logo">
+                logo
+            </div>
+            <nav className="header__nav">
+                nav
+            </nav>
+            <div className="header__cart">
+                cart
+            </div>
+        </header>
+    )
+}
+
+export default Header
