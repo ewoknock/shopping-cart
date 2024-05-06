697e01dd9939a37f519c050ff770de9cb17c4f75
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-05-06 14:08:20 -0700

    Adds background image to home page, text, and button to navitage to shop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom'
 function Header(){
     return (
         <header className="header">
-            <div className="header__logo">
-                <Link to="/">Logo</Link>
-            </div>
-            <nav className="header__nav">
-                <Link to="/">Home</Link>
-                <Link to="/shop">Shop</Link>
-            </nav>
-            <div className="header__cart">
-                <Link to="/cart">Cart</Link>
+            <div className="header__container">
+                <div className="header__logo">
+                    <Link to="/">STAR WARS</Link>
+                </div>
+                <nav className="header__nav">
+                    <Link to="/">Home</Link>
+                    <Link to="/shop">Shop</Link>
+                </nav>
+                <div className="header__cart">
+                    <Link to="/cart">Cart</Link>
+                </div>
             </div>
         </header>
     )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,16 @@
+import Hero from "../assets/star-wars-hero.webp"
+import { Link } from "react-router-dom"
+
 function Home(){
     return (
         <section className="home">
-            Homepage
+            <img src={Hero} />
+            <div className="home__text">
+                <h1>Lego Star Wars Ultimate Collection</h1>
+                <Link to="/shop">
+                    <button className="btn btn__shop">Sets</button>
+                </Link>
+            </div>
         </section>
     )
 }
