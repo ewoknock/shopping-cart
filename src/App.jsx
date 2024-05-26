dabb366603e63eba3293112d779352bd89fd24ba
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-05-26 15:35:10 -0700

    Removed unneeded app.css import

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import {
 import { Outlet } from "react-router-dom";
 import Header from './components/Header';
 import Cart from './components/Cart';
-import './App.css'
 
 function App() {
   const [cart, setCart] = useState([])
